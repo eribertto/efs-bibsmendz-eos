3b08fb55d9cec3280570452f176b15d1f04ca18d
eribertto_archlinux <erimendz@gmail.com>
2023-08-01 05:18:58 +0300

    reduce modeline height to 12

--- init.el
+++ init.el
@@ -106,7 +106,7 @@
 
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+  :custom ((doom-modeline-height 12)))
 
 (use-package which-key
   :defer 0
